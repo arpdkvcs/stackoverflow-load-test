d3e47932c8bee426ae65e655fa6e0d269157cacf
arpdkvcs <arpdkvcs@gmail.com>
2024-03-21 11:50:59 +0100

    Refactor scenarios

--- src/test/java/com/codecool/gatling/GeneralUse.java
+++ src/test/java/com/codecool/gatling/GeneralUse.java
@@ -17,6 +17,9 @@ public class GeneralUse extends Simulation {
     private static final String QUESTION_BODY = "Question for stress testing. What do you think?";
     private static final String ANSWER = "Answer for stress testing. I don't know. Answered by ";
 
+    private static final int MIN_WAIT = 2;
+    private static final int MAX_WAIT = 5;
+
     private HttpProtocolBuilder httpProtocol = http
             .baseUrl("http://localhost:3000")
             .acceptHeader("application/json");
@@ -27,7 +30,7 @@ public class GeneralUse extends Simulation {
         put("content-type", "application/json");
     }};
 
-    private ChainBuilder createQuestion =
+    private ChainBuilder login =
             feed(userFeeder)
                     .exec(http("LogIn").post("/api/auth/login")
                             .headers(sentHeaders)
@@ -35,68 +38,55 @@ public class GeneralUse extends Simulation {
                                     "{" +
                                             "\"username\":\"#{USERNAME}\"," +
                                             "\"password\":\"#{PASSWORD}\"" +
-                                        "}"
+                                            "}"
                             ))
                             .check(status().is(200))
                             .check(jmesPath("data.userid")
                                     .saveAs("userId")))
-                    .pause(2, 5)
+                    .pause(MIN_WAIT, MAX_WAIT)
                     .exec(getCookieValue(CookieKey("jwt")
                             .withSecure(true)))
-                    .exec(addCookie(Cookie("jwt", "#{jwt}")))
-                    .exec(http("Create Question").post("/api/questions")
-                            .headers(sentHeaders)
-                            .body(StringBody(
-                                    "{" +
-                                            "\"userId\":\"#{userId}\"," +
-                                            "\"title\":\"" + QUESTION_TITLE + " #{USERNAME}\"," +
-                                            "\"content\":\"" + QUESTION_BODY + "\"" +
-                                        "}"
-                            ))
-                    )
-                    .pause(2, 5)
-                    .exec(http("LogOut").post("/api/auth/logout"));
+                    .exec(addCookie(Cookie("jwt", "#{jwt}")));
+
+    private ChainBuilder logout =
+            exec(http("LogOut").post("/api/auth/logout"));
+
+    private ChainBuilder createQuestion =
+                exec(http("Create Question").post("/api/questions")
+                        .headers(sentHeaders)
+                        .body(StringBody(
+                                "{" +
+                                        "\"userId\":\"#{userId}\"," +
+                                        "\"title\":\"" + QUESTION_TITLE + " #{USERNAME}\"," +
+                                        "\"content\":\"" + QUESTION_BODY + "\"" +
+                                    "}"
+                        ))
+                )
+                .pause(MIN_WAIT, MAX_WAIT);
 
     private ChainBuilder createAnswer =
-            feed(userFeeder)
-                    .exec(http("LogIn").post("/api/auth/login")
-                            .headers(sentHeaders)
-                            .body(StringBody(
-                                    "{" +
-                                            "\"username\":\"#{USERNAME}\"," +
-                                            "\"password\":\"#{PASSWORD}\"" +
-                                        "}"
-                            ))
-                            .check(jmesPath("data.userid")
-                                    .saveAs("userId"))
-                    )
-                    .pause(2, 5)
-                    .exec(getCookieValue(CookieKey("jwt")
-                            .withSecure(true)))
-                    .exec(addCookie(Cookie("jwt", "#{jwt}")))
-                    .exec(http("Get all questions").get("/api/questions/all")
-                            .check(jsonPath("$.data[*].id")
-                                    .findRandom()
-                                    .saveAs("RANDOM_QUESTION_ID")))
-                    .exec(http("Create Answer").post("/api/answers")
-                            .headers(sentHeaders)
-                            .body(StringBody(
-                                    "{" +
-                                            "\"userId\":\"#{userId}\"," +
-                                            "\"questionId\":\"#{RANDOM_QUESTION_ID}\"," +
-                                            "\"content\":\"" + ANSWER + " #{USERNAME}\"" +
-                                        "}"
-                                    )
-                            )
-                    )
-                    .pause(2, 5)
-                    .exec(http("LogOut").post("/api/auth/logout"));
+                exec(http("Get all questions").get("/api/questions/all")
+                        .check(jsonPath("$.data[*].id")
+                                .findRandom()
+                                .saveAs("RANDOM_QUESTION_ID")))
+                .exec(http("Create Answer").post("/api/answers")
+                        .headers(sentHeaders)
+                        .body(StringBody(
+                                "{" +
+                                        "\"userId\":\"#{userId}\"," +
+                                        "\"questionId\":\"#{RANDOM_QUESTION_ID}\"," +
+                                        "\"content\":\"" + ANSWER + " #{USERNAME}\"" +
+                                    "}"
+                                )
+                        )
+                )
+                .pause(MIN_WAIT, MAX_WAIT);
 
     private ScenarioBuilder newQuestion = scenario("Create new question")
-            .exec(createQuestion);
+            .exec(login, createQuestion, logout);
 
     private ScenarioBuilder newAnswer = scenario("Create new answer")
-            .exec(createAnswer);
+            .exec(login, createAnswer, logout);
 
     {
         setUp(newQuestion.injectOpen(rampUsersPerSec(2).to(5).during(30))
