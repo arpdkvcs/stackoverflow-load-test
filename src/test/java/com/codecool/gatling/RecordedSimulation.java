f78b739f4edd907db05ec0bb79866f213312c788
arpdkvcs <arpdkvcs@gmail.com>
2024-03-22 13:12:04 +0100

    Refactor RecordedSimulation

--- src/test/java/com/codecool/gatling/RecordedSimulation.java
+++ src/test/java/com/codecool/gatling/RecordedSimulation.java
@@ -20,89 +20,35 @@ public class RecordedSimulation extends Simulation {
     .acceptEncodingHeader("gzip, deflate, br")
     .acceptLanguageHeader("en-GB,en-US;q=0.9,en;q=0.8")
     .userAgentHeader("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
-  
-  private Map<CharSequence, String> headers_0 = Map.ofEntries(
-    Map.entry("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"),
-    Map.entry("If-None-Match", "W/\"65f-lAH/PzZZHjVPlUMUQ7092NXB+3E\""),
-    Map.entry("Sec-Fetch-Dest", "document"),
-    Map.entry("Sec-Fetch-Mode", "navigate"),
-    Map.entry("Sec-Fetch-Site", "none"),
-    Map.entry("Sec-Fetch-User", "?1"),
-    Map.entry("Upgrade-Insecure-Requests", "1"),
-    Map.entry("sec-ch-ua", "Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122"),
-    Map.entry("sec-ch-ua-mobile", "?0"),
-    Map.entry("sec-ch-ua-platform", "Linux")
-  );
-  
-  private Map<CharSequence, String> headers_1 = Map.ofEntries(
-    Map.entry("If-Modified-Since", "Sat, 02 Mar 2024 08:39:29 GMT"),
-    Map.entry("If-None-Match", "W/\"1ec-18dfe517c5f\""),
-    Map.entry("Sec-Fetch-Dest", "manifest"),
-    Map.entry("Sec-Fetch-Mode", "cors"),
-    Map.entry("Sec-Fetch-Site", "same-origin"),
-    Map.entry("sec-ch-ua", "Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122"),
-    Map.entry("sec-ch-ua-mobile", "?0"),
-    Map.entry("sec-ch-ua-platform", "Linux")
-  );
-  
-  private Map<CharSequence, String> headers_2 = Map.ofEntries(
-    Map.entry("Content-Type", "application/json"),
-    Map.entry("Sec-Fetch-Dest", "empty"),
-    Map.entry("Sec-Fetch-Mode", "cors"),
-    Map.entry("Sec-Fetch-Site", "same-origin"),
-    Map.entry("sec-ch-ua", "Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122"),
-    Map.entry("sec-ch-ua-mobile", "?0"),
-    Map.entry("sec-ch-ua-platform", "Linux")
-  );
-  
-  private Map<CharSequence, String> headers_3 = Map.ofEntries(
-    Map.entry("Content-Type", "application/json"),
-    Map.entry("Origin", "http://localhost:5000"),
-    Map.entry("Sec-Fetch-Dest", "empty"),
-    Map.entry("Sec-Fetch-Mode", "cors"),
-    Map.entry("Sec-Fetch-Site", "same-origin"),
-    Map.entry("sec-ch-ua", "Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122"),
-    Map.entry("sec-ch-ua-mobile", "?0"),
-    Map.entry("sec-ch-ua-platform", "Linux")
-  );
-
 
   private ScenarioBuilder scn = scenario("RecordedSimulation")
     .exec(
-      http("request_0")
+      http("Main Page")
         .get("/")
-        .headers(headers_0)
         .resources(
-          http("request_1")
-            .get("/manifest.json")
-            .headers(headers_1),
-          http("request_2")
+          http("All questions")
             .get("/api/questions/all")
-            .headers(headers_2)
         ),
-      pause(30),
-      http("request_3")
+      pause(5),
+      http("Sign up")
         .post("/api/auth/register")
-        .headers(headers_3)
-        .body(RawFileBody("com/codecool/gatling/recordedsimulation/0003_request.json")),
-      pause(19),
-      http("request_4")
+        .body(RawFileBody("com/codecool/gatling/recordedsimulation/0003_request.json"))
+              .header("content-type", "application/json"),
+      pause(5),
+      http("Log in")
         .post("/api/auth/login")
-        .headers(headers_3)
         .body(RawFileBody("com/codecool/gatling/recordedsimulation/0004_request.json"))
+              .header("content-type", "application/json")
         .resources(
-          http("request_5")
+          http("All questions")
             .get("/api/questions/all")
-            .headers(headers_2)
         ),
-      pause(11),
-      http("request_6")
+      pause(5),
+      http("Log out")
         .post("/api/auth/logout")
-        .headers(headers_3)
         .resources(
-          http("request_7")
+          http("All questions")
             .get("/api/questions/all")
-            .headers(headers_2)
         )
     );
 
