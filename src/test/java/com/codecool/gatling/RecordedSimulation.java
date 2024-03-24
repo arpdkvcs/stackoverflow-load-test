9c52ae4ad39a2e40a6c9cec9472380a4c9365d8c
arpdkvcs <arpdkvcs@gmail.com>
2024-03-24 11:17:34 +0100

    Clean up the httpProtocol

--- src/test/java/com/codecool/gatling/RecordedSimulation.java
+++ src/test/java/com/codecool/gatling/RecordedSimulation.java
@@ -14,12 +14,7 @@ import static io.gatling.javaapi.jdbc.JdbcDsl.*;
 public class RecordedSimulation extends Simulation {
 
   private HttpProtocolBuilder httpProtocol = http
-    .baseUrl("http://localhost:5000")
-    .inferHtmlResources(AllowList(), DenyList(".*\\.js", ".*\\.css", ".*\\.gif", ".*\\.jpeg", ".*\\.jpg", ".*\\.ico", ".*\\.woff", ".*\\.woff2", ".*\\.(t|o)tf", ".*\\.png", ".*\\.svg", ".*detectportal\\.firefox\\.com.*"))
-    .acceptHeader("*/*")
-    .acceptEncodingHeader("gzip, deflate, br")
-    .acceptLanguageHeader("en-GB,en-US;q=0.9,en;q=0.8")
-    .userAgentHeader("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
+    .baseUrl("http://localhost:5000");
 
   private ScenarioBuilder scn = scenario("RecordedSimulation")
     .exec(
