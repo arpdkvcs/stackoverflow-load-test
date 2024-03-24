a066ce48d6fabb074b1f3bd4af4258abfc3278c5
arpdkvcs <arpdkvcs@gmail.com>
2024-03-24 13:48:59 +0100

    Make all variable static final, and condense code a bit

--- src/test/java/com/codecool/gatling/GeneralUse.java
+++ src/test/java/com/codecool/gatling/GeneralUse.java
@@ -16,7 +16,8 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 public class GeneralUse extends Simulation {
     public static final String DOMAIN = "localhost:3000";
     public static final HttpProtocolBuilder HTTP_PROTOCOL = http.baseUrl("http://" + DOMAIN);
-    private Map<String, String> sentHeaders = new HashMap<>() {{
+    public static final String CSV_FILENAME = "credentials_1000.csv";
+    private static final Map<String, String> SENT_HEADERS = new HashMap<>() {{
         put("content-type", "application/json");
     }};
     private static final String QUESTION_TITLE = "Question from ";
@@ -27,133 +28,123 @@ public class GeneralUse extends Simulation {
     public static final int LOWER_BOUND_OF_USERNAME_POSTFIX = 1;
     public static final int UPPER_BOUND_OF_USERNAME_POSTFIX = 1000;
 
-    private FeederBuilder.Batchable<String> userFeeder = csv("com/codecool/gatling/users/credentials_1000.csv").random();
+    private static final FeederBuilder<String> USER_FEEDER = csv("com/codecool/gatling/users/" + CSV_FILENAME).random();
 
-    private Iterator<Map<String, Object>> randomSearchQueryFeeder =
+    private static final Iterator<Map<String, Object>> RANDOM_SEARCH_QUERY_FEEDER =
             Stream.generate((Supplier<Map<String, Object>>) () -> {
                         String randomSearchQuery = String.valueOf(
                                 ThreadLocalRandom.current()
-                                        .nextInt(LOWER_BOUND_OF_USERNAME_POSTFIX, UPPER_BOUND_OF_USERNAME_POSTFIX + 1)
-                        );
+                                        .nextInt(LOWER_BOUND_OF_USERNAME_POSTFIX, UPPER_BOUND_OF_USERNAME_POSTFIX + 1));
                         return Map.of("query", randomSearchQuery);
-                    }
-            ).iterator();
+            }).iterator();
 
-
-    private ChainBuilder login =
-            feed(userFeeder)
+    private static final ChainBuilder LOGIN =
+            feed(USER_FEEDER)
                     .exec(http("LogIn").post("/api/auth/login")
-                            .headers(sentHeaders)
+                            .headers(SENT_HEADERS)
                             .body(StringBody(
                                     "{" +
                                             "\"username\":\"#{USERNAME}\"," +
                                             "\"password\":\"#{PASSWORD}\"" +
-                                        "}"
-                                    )
-                            )
+                                        "}"))
                             .check(status().is(200))
                             .check(jmesPath("data.userid")
-                                    .saveAs("userId")
-                            )
-                    )
+                                    .saveAs("userId")))
                     .exec(getCookieValue(CookieKey("jwt")
-                            .withSecure(true))
-                    )
-                    .exec(addCookie(Cookie("jwt", "#{jwt}")))
-                    .pause(MIN_WAIT, MAX_WAIT);
+                            .withSecure(true)))
+                    .exec(addCookie(Cookie("jwt", "#{jwt}")));
 
-    private ChainBuilder logout =
-            exec(http("LogOut").post("/api/auth/logout"));
+    private static final ChainBuilder LOGOUT =
+            exec(http("LogOut").post("/api/auth/logout")
+                    .check(status().is(200)));
 
-    private ChainBuilder createQuestion =
+    private static final ChainBuilder CREATE_QUESTION =
             exec(http("Create Question").post("/api/questions")
-                    .headers(sentHeaders)
+                    .headers(SENT_HEADERS)
                     .body(StringBody(
                             "{" +
                                     "\"userId\":\"#{userId}\"," +
                                     "\"title\":\"" + QUESTION_TITLE + " #{USERNAME}\"," +
                                     "\"content\":\"" + QUESTION_BODY + "\"" +
-                                "}"
-                            )
-                    )
-                    .check(status().is(200))
-            )
-            .pause(MIN_WAIT, MAX_WAIT);
+                                "}"))
+                    .check(status().is(200)));
 
-    private ChainBuilder getAllQuestions =
-            exec(http("Get all questions").get("/api/questions/all")
-                    .check(jsonPath("$.data[*].id")
-                            .findRandom()
-                            .saveAs("RANDOM_QUESTION_ID")
-                    )
-            )
-            .pause(MIN_WAIT, MAX_WAIT);
-
-    private ChainBuilder openQuestionDetails =
-            exec(http("Open Question Details").get("/api/questions/#{RANDOM_QUESTION_ID}"))
-            .pause(MIN_WAIT, MAX_WAIT);
-
-    private ChainBuilder createAnswer =
+    private static final ChainBuilder CREATE_ANSWER =
             exec(http("Create Answer").post("/api/answers")
-                    .headers(sentHeaders)
+                    .headers(SENT_HEADERS)
                     .body(StringBody(
                             "{" +
                                     "\"userId\":\"#{userId}\"," +
                                     "\"questionId\":\"#{RANDOM_QUESTION_ID}\"," +
                                     "\"content\":\"" + ANSWER + " #{USERNAME}\"" +
-                                "}"
-                            )
-                    )
-                    .check(status().is(200))
-            )
-            .pause(MIN_WAIT, MAX_WAIT);
+                                "}"))
+                    .check(status().is(200)));
 
-    private ChainBuilder search =
-            feed(randomSearchQueryFeeder)
+    private static final ChainBuilder GET_ALL_QUESTIONS =
+            exec(http("Get all questions").get("/api/questions/all")
+                    .check(jsonPath("$.data[*].id")
+                            .findRandom()
+                            .saveAs("RANDOM_QUESTION_ID"))
+                    .check(status().is(200)));
+
+    private static final ChainBuilder OPEN_A_QUESTIONS_DETAILS =
+            exec(http("Open Question Details").get("/api/questions/#{RANDOM_QUESTION_ID}")
+                    .check(status().is(200)));
+
+    private static final ChainBuilder SEARCH =
+            feed(RANDOM_SEARCH_QUERY_FEEDER)
                     .exec(http("Search Questions").get("/api/questions/search/#{query}")
                             .check(jsonPath("$.data[*].id")
                                     .findRandom()
-                                    .saveAs("RANDOM_QUESTION_ID")
-                            )
-                    )
-                    .pause(MIN_WAIT, MAX_WAIT)
-                    .exec(openQuestionDetails);
+                                    .saveAs("RANDOM_QUESTION_ID"))
+                            .check(status().is(200)));
 
-    private ChainBuilder browse =
+    private static final ChainBuilder BROWSE_ALL_QUESTIONS =
             repeat(3).on(
                     pace(MIN_WAIT, MAX_WAIT)
-                            .exec(getAllQuestions)
-                            .exec(openQuestionDetails)
-                    )
-                    .pause(MIN_WAIT, MAX_WAIT)
-                    .exec(search)
-                    .pause(MIN_WAIT, MAX_WAIT);
+                            .exec(GET_ALL_QUESTIONS)
+                            .exec(OPEN_A_QUESTIONS_DETAILS));
 
+    private static final ScenarioBuilder POST_NEW_QUESTION = scenario("Post new question")
+            .exec(LOGIN)
+            .pause(MIN_WAIT, MAX_WAIT)
+            .exec(CREATE_QUESTION)
+            .pause(MIN_WAIT, MAX_WAIT)
+            .exec(LOGOUT);
 
-    private ScenarioBuilder postingQuestions = scenario("Post new question")
-            .exec(login, createQuestion, logout);
+    private static final ScenarioBuilder ANSWER_A_QUESTION = scenario("Answer a question")
+            .exec(LOGIN)
+            .pause(MIN_WAIT, MAX_WAIT)
+            .exec(BROWSE_ALL_QUESTIONS)
+            .pause(MIN_WAIT, MAX_WAIT)
+            .exec(SEARCH)
+            .pause(MIN_WAIT, MAX_WAIT)
+            .exec(OPEN_A_QUESTIONS_DETAILS)
+            .pause(MIN_WAIT, MAX_WAIT)
+            .exec(CREATE_ANSWER)
+            .pause(MIN_WAIT, MAX_WAIT)
+            .exec(LOGOUT);
 
-    private ScenarioBuilder answeringQuestions = scenario("Answer a question")
-            .exec(login, browse, createAnswer, logout);
-
-    private ScenarioBuilder browsingQuestions = scenario("Browse content")
-            .exec(browse);
+    private static final ScenarioBuilder BROWSE_CONTENT = scenario("Browse content")
+            .exec(BROWSE_ALL_QUESTIONS)
+            .pause(MIN_WAIT, MAX_WAIT)
+            .exec(SEARCH)
+            .pause(MIN_WAIT, MAX_WAIT)
+            .exec(OPEN_A_QUESTIONS_DETAILS);
 
     {
-        setUp(postingQuestions.injectOpen(
+        setUp(
+                POST_NEW_QUESTION.injectOpen(
                     atOnceUsers(2),
-                    rampUsersPerSec(1).to(5).during(30).randomized()
-                ),
-                answeringQuestions.injectOpen(
+                    rampUsersPerSec(1).to(5).during(30).randomized()),
+                ANSWER_A_QUESTION.injectOpen(
                     nothingFor(5),
                     atOnceUsers(10),
                     nothingFor(2),
-                    rampUsersPerSec(1).to(10).during(40).randomized()
-                ),
-                browsingQuestions.injectOpen(
+                    rampUsersPerSec(1).to(10).during(40).randomized()),
+                BROWSE_CONTENT.injectOpen(
                     nothingFor(15),
-                    rampUsersPerSec(5).to(10).during(80).randomized()
-                )
+                    rampUsersPerSec(5).to(10).during(80).randomized())
         ).protocols(HTTP_PROTOCOL);
     }
 }
